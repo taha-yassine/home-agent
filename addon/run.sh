309c8d1dbad4b151cedf6c198d628f4f9c968a46
Taha YASSINE <yassinetaha1997@gmail.com>
2025-06-22 06:16:10 +0100

    Set up add-on requirements

---
+++ addon/run.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/with-contenv bashio
+
+bashio::log.info "Starting Home Agent addon..."
+
+export HOME_AGENT_LLM_SERVER_URL=$(bashio::config 'llm_server_url')
+export HOME_AGENT_LLM_SERVER_API_KEY=$(bashio::config 'llm_server_api_key')
+export HOME_AGENT_LLM_SERVER_PROXY=$(bashio::config 'llm_server_proxy')
+
+bashio::log.info "Starting Uvicorn server."
+
+exec uv run uvicorn app:app --host 0.0.0.0 --port 8000
