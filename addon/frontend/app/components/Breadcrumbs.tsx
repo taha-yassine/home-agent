031b0a24bb054ea45f20be38738af546edb8e02d
Taha YASSINE <yassinetaha1997@gmail.com>
2025-07-24 20:42:55 +0100

    Improve breadcrumbs

--- addon/frontend/app/components/Breadcrumbs.tsx
+++ addon/frontend/app/components/Breadcrumbs.tsx
@@ -10,31 +10,29 @@ export default function Breadcrumbs() {
   }
 
   return (
-    <nav className="flex mb-4" aria-label="Breadcrumb">
+    <nav className="flex" aria-label="Breadcrumb">
       <ol className="inline-flex items-center">
         {pathnames.map((value, index) => {
           const to = `/${pathnames.slice(0, index + 1).join("/")}`;
           const isLast = index === pathnames.length - 1;
 
           let name = value;
-          if (index === 0 && value === "conversations") {
-            name = "Conversations";
-          }
+          name = value.charAt(0).toUpperCase() + value.slice(1);
 
           return (
             <li key={to}>
               <div className="flex items-center">
                 {index > 0 && (
-                  <Slash className="mx-3 w-4 h-4 text-gray-400" />
+                  <Slash className="mx-3 w-4 h-4 text-zinc-400 dark:text-zinc-500" />
                 )}
                 {isLast ? (
-                  <span className="text-sm font-medium text-gray-500">
+                  <span className="text-sm font-medium text-zinc-500 dark:text-zinc-400">
                     {name}
                   </span>
                 ) : (
                   <Link
                     to={to}
-                    className="text-sm font-medium text-gray-700 hover:text-gray-900"
+                    className="text-sm font-medium text-zinc-700 hover:text-zinc-900 dark:text-zinc-300 dark:hover:text-white"
                   >
                     {name}
                   </Link>
