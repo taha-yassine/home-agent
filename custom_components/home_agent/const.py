2c65df91df9304db8f4563113a529a1b75e16fa7
Taha YASSINE <yassinetaha1997@gmail.com>
2025-08-28 00:34:55 +0200

    Use correct add-on hostname

--- custom_components/home_agent/const.py
+++ custom_components/home_agent/const.py
@@ -2,7 +2,11 @@
 
 DOMAIN = "home_agent"
 
-ADDON_URL = "http://localhost:8000"  # TODO: change to the actual addon url; see https://developers.home-assistant.io/docs/add-ons/communication#network
+# Generated using hashlib.sha1("https://github.com/taha-yassine/home-agent".lower().encode()).hexdigest()[:8]
+# See https://developers.home-assistant.io/docs/add-ons/communication#network
+# TODO: Avoid hardcoding/make it configurable
+# TODO: Potentially implement discovery
+ADDON_URL = "9bd46c1b-home-agent"
 
 DEFAULT_MAX_HISTORY = 10  # Number of messages to keep in history
 MAX_HISTORY_SECONDS = 600  # How long to keep conversation history (10 minutes)
