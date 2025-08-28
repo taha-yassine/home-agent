8d7519ce3d3af50429f559d4eab22d6a3343241b
Taha YASSINE <yassinetaha1997@gmail.com>
2025-08-28 02:22:55 +0200

    Track missing file

---
+++ addon/frontend/app/routes/usage.tsx
@@ -0,0 +1,12 @@
+import Breadcrumbs from "../components/Breadcrumbs";
+
+export default function Usage() {
+  return (
+    <>
+      <div className="flex justify-between items-center mb-4 min-h-10">
+        <Breadcrumbs />
+      </div>
+      <h2>Usage</h2>
+    </>
+  );
+} 
